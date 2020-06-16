5215a908751c8a47ee79617bf435e51f8d58febb
Stephen <stephen_h369@live.co.uk>
2020-06-16 16:19:38 +0100

    test add string as number passes

--- ruby_functions_practice.rb
+++ ruby_functions_practice.rb
@@ -22,3 +22,10 @@ def length_of_string(test_string)
     return test_string.length 
 end
 
+def join_string(string_1, string_2)
+    return string_1 + string_2
+end
+
+def add_string_as_number(string_1, string_2)
+    return string_1.to_i + string_2.to_i
+end
